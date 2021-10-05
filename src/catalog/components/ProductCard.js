86dc2b4cf3c287bf9ed87c966658b4d6071304e9
DanielChungara <danielchungara1@gmail.com>
2021-10-05 00:31:19 -0300

    Card styles.

--- src/catalog/components/ProductCard.js
+++ src/catalog/components/ProductCard.js
@@ -12,37 +12,73 @@ const ProductCard = ({ product }) => {
 
     const [isModalVisible, showModal, handleOk, handleCancel] = useModal();
 
-    return (        
-            <Card
-                hoverable
-                onClickCapture={showModal}                          
-                style={{ height: '100%'}}
-                cover={<img alt="example" src={imageUrlMd} style={{padding: '5px' }}/>}
-            >
-                <Meta
-                    title={description}
-                />
-                <CurrencyFormat
-                    value={price}
-                    displayType={'text'}
-                    prefix={'$'}
-                    style={{ lineHeight: '2rem'}}
-                />
-                {stock > 0
-                    ? (<div>Available</div>)
-                    : (<div>Unavailable</div>)
-                }
-                <Modal
-                    title="Product Detail"
-                    visible={isModalVisible}
-                    onOk={handleOk}
-                    onCancel={handleCancel}
-                >
-                    <ProductDetail product={product} />
-                </Modal>
+    const stockLabel = (<>
+        {stock > 0
+            ? <span style={{ ...SAvailable, ...SAvailability }}>In Stock</span>
+            : <span style={{ ...SUnavailable, ...SAvailability }}>Out Stock</span>
+        }
+    </>);
 
-            </Card>    
+    const productImage = (
+        <img
+            alt="example"
+            src={imageUrlMd}
+            style={{...SProductImage}}
+            onClick={showModal}
+        />
+    );
+
+    return (
+        <Card
+            style={{ height: '100%' }}
+            cover={productImage}
+            extra={stockLabel}            
+        >
+            <Meta
+                title={description}
+            />
+            <CurrencyFormat
+                value={price}
+                displayType={'text'}
+                prefix={'$'}
+                style={SPrice}
+            />
+            <Modal
+                title="Product Detail"
+                visible={isModalVisible}
+                onOk={handleOk}
+                onCancel={handleCancel}
+            >
+                <ProductDetail product={product} />
+            </Modal>
+
+        </Card>
     );
 };
 
 export default ProductCard;
+
+// ***************************************************************************************************
+// * Styles *
+// ***************************************************************************************************
+const SAvailability = {
+    borderColor: '#4caf50',
+    borderRadius: '5px',
+    padding: '5px 10px',
+    color: '#fff',
+};
+const SAvailable = {
+    backgroundColor: '#4caf50'
+};
+const SUnavailable = {
+    backgroundColor: '#FF4F4B'
+};
+const SPrice = {
+    fontSize: '1.3rem',
+    fontWeight: '600',
+    lineHeight: '3rem'
+}
+const SProductImage = {
+    padding: '5px',
+    cursor: 'pointer'
+}
